d3251a9db044ad902480340ba444f9f44364bf0a
Charlie Phillips <hi.charliephillips@gmail.com>
2020-10-20 21:54:35 -0500

    added simple fan rotation script

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeed = 20;
+
+    private void Update()
+    {
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+}
