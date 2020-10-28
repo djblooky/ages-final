2c94667d93e4c87162f6e563b607d1c77cdfeb1c
Charlie Phillips <hi.charliephillips@gmail.com>
2020-10-27 21:22:35 -0500

    level resets when timer runs out

---
+++ Assets/Scripts/ResetLevel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetLevel : MonoBehaviour
+{
+    [SerializeField]
+    private float delayBeforeResetLevel = 5f;
+
+    private void OnTimerRanOut()
+    {
+        //TODO: display death message
+        StartCoroutine(ResetSceneAfterDelay());
+    }
+
+    private IEnumerator ResetSceneAfterDelay()
+    {
+        yield return new WaitForSeconds(delayBeforeResetLevel);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnEnable()
+    {
+        DeathTimer.TimerRanOut += OnTimerRanOut;
+    }
+
+    private void OnDisable()
+    {
+        DeathTimer.TimerRanOut -= OnTimerRanOut;
+    }
+}
