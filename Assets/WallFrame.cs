0b49ffec39d714fa5f3c724de9a59a32aa845a24
Charlie Phillips <hi.charliephillips@gmail.com>
2020-11-05 15:34:24 -0600

    interactable wall frame

---
+++ Assets/WallFrame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallFrame : Interactable
+{
+    private Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public override void Interact()
+    {
+        rigidBody.useGravity = true;
+    }
+}
