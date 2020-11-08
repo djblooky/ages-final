b9f9cd91fd5dc1c7a81539d3574289ea63968ce2
Charlie Phillips <hi.charliephillips@gmail.com>
2020-11-08 17:11:29 -0600

    created toggleable base class

---
+++ Assets/Scripts/InteractSystem/Interactables/Toggleable.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Toggleable : Interactable
+{
+    protected bool isOn;
+
+    public override void Interact()
+    {
+        Toggle();
+    }
+
+    protected override void OnHoveredOver(Interactable i)
+    {
+        SetHoverText();
+    }
+
+    private void SetHoverText()
+    {
+        if (isOn)
+        {
+            interactText = "Turn off";
+        }
+        else
+        {
+            interactText = "Turn on";
+        }
+    }
+
+    private void Toggle()
+    {
+        if (isOn)
+        {
+            isOn = false;
+        }
+        else
+        {
+            isOn = true;
+        }
+    }
+
+}
