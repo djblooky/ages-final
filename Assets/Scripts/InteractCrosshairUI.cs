df4ed0f32c53c17db5a0d1a7b51d6ed344880d36
Charlie Phillips <hi.charliephillips@gmail.com>
2020-11-08 19:09:03 -0600

    added hover crosshair

---
+++ Assets/Scripts/InteractCrosshairUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractCrosshairUI : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite defaultCrosshair, interactCrosshair;
+
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    private void OnHoveredOverInteractable(Interactable i)
+    {
+        image.sprite = interactCrosshair;
+    }
+
+    private void OnHoveredOffInteractable()
+    {
+        image.sprite = defaultCrosshair;
+    }
+
+    private void OnEnable()
+    {
+        PlayerRaycast.HoveredOver += OnHoveredOverInteractable;
+        PlayerRaycast.HoveredOff += OnHoveredOffInteractable;
+    }
+
+    private void OnDisable()
+    {
+        PlayerRaycast.HoveredOver -= OnHoveredOverInteractable;
+        PlayerRaycast.HoveredOff -= OnHoveredOffInteractable;
+    }
+}
