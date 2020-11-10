b3202dbc5723b8b6f7f3a3b0437d22b246826283
Charlie Phillips <hi.charliephillips@gmail.com>
2020-11-10 01:19:10 -0600

    added phone and candle interactables

---
+++ Assets/Scripts/InteractSystem/Interactables/Candle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Candle : Toggleable
+{
+    [SerializeField]
+    private ParticleSystem flameParticle;
+
+    public override void Interact()
+    {
+        base.Interact();
+        ToggleCandle();
+    }
+
+    private void ToggleCandle()
+    {
+        if (isOn)
+        {
+            flameParticle.Play();
+        }
+        else
+        {
+            flameParticle.Stop();
+        }
+    }
+}
