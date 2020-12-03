f67e0102a0baccb8e2e7db3a35a1ac8cefdf68fb
Charlie Phillips <hi.charliephillips@gmail.com>
2020-12-03 15:20:44 -0600

    bug fixes for build

--- Assets/EscapeToQuit.cs
+++ Assets/EscapeToQuit.cs
@@ -6,7 +6,13 @@ public class EscapeToQuit : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Application.Quit();
+#if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
         }
     }
 }
