d25816f35f63dd63b64cce7d4073e4182be6cb37
Charlie Phillips <hi.charliephillips@gmail.com>
2020-12-10 18:39:06 -0600

    created title screen scene/layout

---
+++ Assets/Scripts/FlickerLights.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLights : MonoBehaviour
+{
+    [SerializeField]
+    private Light[] lightsToFlicker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
